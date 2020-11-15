02522f244a49a68a62a81d3684126a37fef53c2f
Delph <delph_teb@hotmail.com>
2020-11-14 23:57:51 -0500

    add countdown and start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,30 @@
-// Countdown timer
-const startingMinutes = 5;
-var time = startingMinutes * 60;
+const startButton = document.querySelector("#start");
+const questionContainerElement = document.querySelector("#question-container")
+const countdownBox = document.querySelector(".clock");
 
-const countdownBox = document.getElementById("clock");
+const startingMinutes = 5;          // Initial timer in minutes
+var time = startingMinutes * 60;     // conversion of minutes into seconds
 
+startButton.addEventListener("click", function () {
+  startGame();
+  startClock()
+});
 
-function timerUpdate() {
+function startClock() {
+  setInterval(function(){                           // Interval time of clock at every 1000 millisecond
     var minutes = Math.floor(time / 60);
     var seconds = time % 60;
-   // Adding leading 0
+    // Adding leading 0
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
-   
-    countdownBox.textContent = `${minutes}:${seconds}`;
+    countdownBox.value = `${minutes}:${seconds}`;
     time--;
+  }, 1000);  
+  
+    
 }
 
-setInterval(timerUpdate, 1000);  // Interval time at every 1000 millisecond
 
+function startGame(){
+
+}
