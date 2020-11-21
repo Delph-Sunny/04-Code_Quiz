2c04be6862edfccf6ce43ea07e0519507d0282f1
Delph <delph_teb@hotmail.com>
2020-11-21 18:20:03 -0500

    add if loop for better high scores display

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -5,33 +5,34 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const ranking = ["1st", "2nd", "3rd", "4th", "5th"]
 
  
-/**** Create table content ****/
-/* Display header */
-var thead = document.createElement("thead");
-table.appendChild(thead);
-var rowHeader = thead.insertRow(0);
-rowHeader.insertCell(0).outerHTML= "<th>Rank</th>";
-rowHeader.insertCell(1).outerHTML= "<th>Name</th>";
-rowHeader.insertCell(2).outerHTML= "<th>Score</th>";
+/**** Create table content when recorded scores****/
+if (highScores.length !== 0) {
+  /* Display header */
+  var thead = document.createElement("thead");
+  table.appendChild(thead);
+  var rowHeader = thead.insertRow(0);
+  rowHeader.insertCell(0).outerHTML= "<th>Rank</th>";
+  rowHeader.insertCell(1).outerHTML= "<th>Name</th>";
+  rowHeader.insertCell(2).outerHTML= "<th>Score</th>";
 
-/* Display results */
-var tbody = document.createElement("tbody");
-table.appendChild(tbody);
-for (let i=0; i < highScores.length; i++ ){
-  let row = tbody.insertRow(i);
-  let cell1 = row.insertCell(0);
-  let cell2 = row.insertCell(1);
-  let cell3 = row.insertCell(2);
+  /* Display results */
+  var tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+  for (let i=0; i < highScores.length; i++ ){
+    let row = tbody.insertRow(i);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
 
-  let name = highScores[i].name;
-  let score = highScores[i].score;
-  let rank = ranking[i]; 
+    let name = highScores[i].name;
+    let score = highScores[i].score;
+    let rank = ranking[i]; 
   
-  cell1.innerHTML=`${rank}`;
-  cell2.innerHTML=`${name}`;
-  cell3.innerHTML=`${score}`;
+    cell1.innerHTML=`${rank}`;
+    cell2.innerHTML=`${name}`;
+    cell3.innerHTML=`${score}`;
+  }
 }
-
 
 /**** Clear all results from screen and storage ****/
 clearBtn.addEventListener("click", function(event){
